5828e371927273f21267e30ffa45df1b69f73149
Daniel (aka. rotgruengelb) <rotgrungelblab@gmail.com>
2024-09-21 17:29:45 +0200

    Improved typing and added doc string.

--- scripts/minecraft_resources.py
+++ scripts/minecraft_resources.py
@@ -3,21 +3,26 @@ import os
 import shutil
 import zipfile
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import Any
+
 import requests
+
 from timing import time_this
 
 BUILD_PROCESS_DIR = "../build/process"
 
+
 @time_this
-def prepare_resources_references(mc_versions: list) -> bool:
+def prepare_resources_references(mc_versions: list[str]) -> bool:
     if not download_client_jars(mc_versions):
         return False
     unzip_client_jars(mc_versions)
     clean_process_version_directories(mc_versions)
     return True
 
+
 @time_this
-def download_client_jars(mc_versions: list) -> bool:
+def download_client_jars(mc_versions: list[str]) -> bool:
     manifest = get_manifest()
     if len(manifest) == 0:
         return False
@@ -28,8 +33,9 @@ def download_client_jars(mc_versions: list) -> bool:
                 return False
     return True
 
+
 @time_this
-def get_manifest() -> dict:
+def get_manifest() -> dict[str, Any]:
     manifest_url = "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json"
     manifest_response = requests.get(manifest_url)
     if manifest_response.status_code != 200:
@@ -38,7 +44,8 @@ def get_manifest() -> dict:
     logging.info("Fetched manifest")
     return manifest_response.json()
 
-def download_client_jar(mc_version: str, manifest: dict) -> bool:
+
+def download_client_jar(mc_version: str, manifest: dict[str, Any]) -> bool:
     version_field = next((v for v in manifest["versions"] if v["id"] == mc_version), None)
     if not version_field:
         logging.error(f"Version {mc_version} not found in manifest")
@@ -63,37 +70,41 @@ def download_client_jar(mc_version: str, manifest: dict) -> bool:
     logging.info(f"Downloaded {download_path}")
     return True
 
+
 @time_this
-def unzip_client_jars(mc_versions: list):
+def unzip_client_jars(mc_versions: list[str]) -> None:
     with ThreadPoolExecutor() as executor:
         futures = [executor.submit(unzip_client_jar, mc_version) for mc_version in mc_versions]
         for future in as_completed(futures):
             future.result()
 
-def unzip_client_jar(mc_version: str):
-        version_dir = os.path.join(BUILD_PROCESS_DIR, mc_version)
-        jar_path = os.path.join(version_dir, "client.jar")
-        if os.path.exists(jar_path):
-            with zipfile.ZipFile(jar_path, 'r') as zip_ref:
-                zip_ref.extractall(version_dir)
-            logging.info(f"Unzipped {jar_path}")
+
+def unzip_client_jar(mc_version: str) -> None:
+    version_dir = os.path.join(BUILD_PROCESS_DIR, mc_version)
+    jar_path = os.path.join(version_dir, "client.jar")
+    if os.path.exists(jar_path):
+        with zipfile.ZipFile(jar_path, 'r') as zip_ref:
+            zip_ref.extractall(version_dir)
+        logging.info(f"Unzipped {jar_path}")
+
 
 @time_this
-def clean_process_version_directories(mc_versions: list):
+def clean_process_version_directories(mc_versions: list[str]) -> None:
     with ThreadPoolExecutor() as executor:
         futures = [executor.submit(clean_process_version_directory, mc_version) for mc_version in mc_versions]
         for future in as_completed(futures):
             future.result()
 
-def clean_process_version_directory(mc_version: str):
-        version_dir = os.path.join(BUILD_PROCESS_DIR, mc_version)
-        for root, dirs, files in os.walk(version_dir):
-            for name in files:
-                if not (name.startswith("assets" + os.sep) and name.startswith("data" + os.sep)):
-                    file_path = os.path.join(root, name)
-                    os.remove(file_path)
-            for name in dirs:
-                if not (name == "assets" or name == "data"):
-                    dir_path = os.path.join(root, name)
-                    shutil.rmtree(dir_path)
-        logging.info(f"Cleaned {version_dir}")
+
+def clean_process_version_directory(mc_version: str) -> None:
+    version_dir = os.path.join(BUILD_PROCESS_DIR, mc_version)
+    for root, dirs, files in os.walk(version_dir):
+        for name in files:
+            if not (name.startswith("assets" + os.sep) and name.startswith("data" + os.sep)):
+                file_path = os.path.join(root, name)
+                os.remove(file_path)
+        for name in dirs:
+            if not (name == "assets" or name == "data"):
+                dir_path = os.path.join(root, name)
+                shutil.rmtree(dir_path)
+    logging.info(f"Cleaned {version_dir}")
--- scripts/timing.py
+++ scripts/timing.py
@@ -1,12 +1,24 @@
 import logging
 import time
 
+from typing import Callable, Any
 
-def time_this(func):
-    def wrapper(*args, **kwargs):
+
+def time_this(func: Callable) -> Callable:
+    """
+    A decorator that logs the time a function takes to execute.
+
+    Args:
+        func (Callable): The function to be timed.
+
+    Returns:
+        Callable: The wrapped function with timing functionality.
+    """
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
         logging.info(f"Function {func.__name__} took {end_time - start_time:.2f} seconds")
         return result
+
     return wrapper
