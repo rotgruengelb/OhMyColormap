cfa7294d10cd130bbce8dcc21b993b990cf9f66b
Daniel (aka. rotgruengelb) <rotgrungelblab@gmail.com>
2024-09-21 11:07:16 +0200

    Added complete resource preparation process

---
+++ scripts/timing.py
@@ -0,0 +1,12 @@
+import logging
+import time
+
+
+def time_this(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        logging.info(f"Function {func.__name__} took {end_time - start_time:.2f} seconds")
+        return result
+    return wrapper
