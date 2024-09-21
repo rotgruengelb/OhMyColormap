a7361fe84fb1a1dc79b93186cf5c4b88a6f3357e
Daniel (aka. rotgruengelb) <rotgrungelblab@gmail.com>
2024-09-21 17:47:09 +0200

    renamed structure_copy parms for clarity.

--- scripts/util.py
+++ scripts/util.py
@@ -2,19 +2,19 @@ import os
 import shutil
 
 
-def structure_copy(source_dir: os.PathLike, source_file: os.PathLike, dest_dir: os.PathLike) -> None:
+def structure_copy(scr_root_dir: os.PathLike, scr_file_path: os.PathLike, dst_root_dir: os.PathLike) -> None:
     """
     Copies a file from a source directory to a destination directory, preserving the directory structure.
 
     Args:
-        source_dir (os.PathLike): The root directory of the source file.
-        source_file (os.PathLike): The path to the source file to be copied.
-        dest_dir (os.PathLike): The root directory where the file should be copied to.
+        scr_root_dir (os.PathLike): The root directory of the directory structure to be preserved.
+        scr_file_path (os.PathLike): The path to the file to be copied.
+        dst_root_dir (os.PathLike): The root directory where the file should be copied to.
 
     Returns:
         None
     """
-    relative_path = os.path.relpath(source_file, start=source_dir)
-    dest_path = os.path.join(dest_dir, relative_path)
+    relative_path = os.path.relpath(scr_file_path, start=scr_root_dir)
+    dest_path = os.path.join(dst_root_dir, relative_path)
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-    shutil.copy2(source_file, dest_path)
+    shutil.copy2(scr_file_path, dest_path)
