4b3ef81564d0b6523d3e5ce57ac8e47e2da8524f
Daniel (aka. rotgruengelb) <rotgrungelblab@gmail.com>
2024-11-01 01:39:30 +0100

    draft?

--- build.py
+++ build.py
@@ -1,2 +1,18 @@
+color_sets = [
+    {
+        "version": 1,
+        "name": "bi",
+        "style": "tricolor",
+        "colors": ["d60270", "9b4f96", "0038a8"]
+    }
+]
+
+styles = {
+    "tricolor": {
+        "version": 1,
+        "expects": 3
+    }
+}
+
 if __name__ == '__main__':
     ...
