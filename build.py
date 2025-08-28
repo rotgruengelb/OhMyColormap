1661b11c6ae25023db090835f7389ae69c61dd3d
Daniel (rotgruengelb) <rotgruengelb@rotgruengelb.net>
2025-08-28 21:29:55 +0200

    chore: move /util/assets to /src/resources

--- build.py
+++ build.py
@@ -55,7 +55,7 @@ def main():
             # Save images based on style configuration
             if 'merge_background_into_frame' in style_config and style_config['merge_background_into_frame']:
                 background_frame_image.save(tooltip_path / 'frame.png')
-                shutil.copytree(Path('util/assets/tooltip_use_only_frame'), tooltip_path, dirs_exist_ok=True)
+                shutil.copytree(Path('src/resources/tooltip_use_only_frame'), tooltip_path, dirs_exist_ok=True)
             else:
                 background_image.save(tooltip_path / 'background.png')
                 frame_image.save(tooltip_path / 'frame.png')
@@ -63,11 +63,11 @@ def main():
             # Generate `pack.mcmeta` and add common files
             pack_description = f"Pride Tooltips | {palette['description_name']} {style_config['description_name']}"
             create_pack_metadata(build_out_path / 'pack.mcmeta', pack_description)
-            shutil.copytree(Path('util/assets/tooltip_common'), tooltip_path, dirs_exist_ok=True)
+            shutil.copytree(Path('src/resources/tooltip_common'), tooltip_path, dirs_exist_ok=True)
 
             # Generate `pack.png`
             base_icon_image = nine_slice_scale(background_frame_image, 2, 2, 2, 2, 51, 36, False, (8, 8, 8, 8))
-            text_overlay_image = Image.open(Path('util/assets/pack_png_tooltip_text.png'))
+            text_overlay_image = Image.open(Path('src/resources/pack_png_tooltip_text.png'))
             unscaled_icon_image = Image.alpha_composite(base_icon_image, text_overlay_image)
             new_size = (unscaled_icon_image.width * 6, unscaled_icon_image.height * 6)
             scaled_image = unscaled_icon_image.resize(new_size, Resampling.NEAREST)
